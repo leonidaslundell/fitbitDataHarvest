30e6fefa90af18ae9b2d258067ea601a0d7b4525
leonidaslundell <leonidas.lundell@gmail.com>
2022-01-05 20:30:16 +0100

    Working version 1

--- R/fitbitAPI.R
+++ R/fitbitAPI.R
@@ -1,28 +1,32 @@
 #persistent activation code for generating access tokens
-activationCode <- function(code){
-  httr::POST(url = "https://api.fitbit.com/oauth2/token", 
-             httr::add_headers("Authorization" = " Basic MjM4M0Q2OjdlYjdlZDQ5NGJjMWFkN2FjMGFlZGE0MWJlODk4Zjhj",
-                               "Content-Type" = "application/x-www-form-urlencoded"),
-             body = list("clientId" = "2383D6",
-                         "grant_type" = "authorization_code",
-                         "redirect_uri" = "https://localhost:1410/",
-                         "code" = code),
-             encode = "form") |> 
+activationCode <- function(code, clientID){
+  x <- httr::POST(url = "https://api.fitbit.com/oauth2/token", 
+                  httr::add_headers("Authorization" = " Basic MjM4M0Q2OjdlYjdlZDQ5NGJjMWFkN2FjMGFlZGE0MWJlODk4Zjhj",
+                                    "Content-Type" = "application/x-www-form-urlencoded"),
+                  body = list("clientId" = clientID,
+                              "grant_type" = "authorization_code",
+                              "redirect_uri" = "https://localhost:1410/",
+                              "expires_in" = "259200",
+                              "code" = code),
+                  encode = "form") |> 
     httr::content(as = "text") |>
     jsonlite::fromJSON(simplifyVector = TRUE)
+  x[["refresh_token"]]
 }
 
 #use access code for generating an access token
-accessToken <- function(refresh){
-  httr::POST(url = "https://api.fitbit.com/oauth2/token", 
+accessToken <- function(refresh, clientID){
+  x <- httr::POST(url = "https://api.fitbit.com/oauth2/token", 
              httr::add_headers("Authorization" = " Basic MjM4M0Q2OjdlYjdlZDQ5NGJjMWFkN2FjMGFlZGE0MWJlODk4Zjhj",
                                "Content-Type" = "application/x-www-form-urlencoded"),
-             body = list("clientId" = "2383D6",
+             body = list("clientId" = clientID,
                          "grant_type" = "refresh_token",
-                         "refresh_token" = refresh),
+                         "refresh_token" = refresh,
+                         "expires_in" = "31560000"),
              encode = "form") |> 
     httr::content(as = "text") |>
     jsonlite::fromJSON(simplifyVector = TRUE)
+  x[["access_token"]]
 }
 
 #get data for 
@@ -39,6 +43,6 @@ getData <- function(token,
     jsonlite::fromJSON(simplifyVector = TRUE)
 }
 
-refrToken <- activationCode("24fd274a42962b8db197a353709dcf5a5890d1d8")
-actiToken <- accessToken(refrToken$refresh_token)
-getData(actiToken$access_token, startDate = "2021-12-20")
+# refrToken <- activationCode("18f11e2596a6cf799115c7ba41073a6da8f11a0c")
+# actiToken <- accessToken("e4e50c7a0ffd1a809426a4710ab99399f621560b08e7997322a69e115ca5bf56")
+# getData(actiToken$access_token, startDate = "2021-12-29")

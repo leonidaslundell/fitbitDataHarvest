00dc0d11552302bcd5542744c1301e0ea7e3c7be
leonidaslundell <leonidas.lundell@gmail.com>
2022-01-06 15:55:34 +0100

    added extra data sources from the api

--- R/fitbitAPI.R
+++ R/fitbitAPI.R
@@ -39,19 +39,19 @@ getData <- function(token,
                          "minutesSedentary", 
                          "minutesLightlyActive", 
                          "minutesFairlyActive", 
-                         "minutesVeryActive"), \(x){
+                         "minutesVeryActive"), \(act){
              Sys.sleep(.2)
              url <- paste0("https://api.fitbit.com/1/user/-/activities/",
-                           x,
+                           act,
                            "/date/",
                            startDate,
                            "/",
                            finishDate,
                            ".json")
-             x <- httr::GET(url = url, httr::add_headers("Authorization" = paste("Bearer", token))) |>
+             act <- httr::GET(url = url, httr::add_headers("Authorization" = paste("Bearer", token))) |>
                httr::content(as = "text") |>
                jsonlite::fromJSON(simplifyVector = TRUE)
-             x[[1]]
+             act[[1]]
            })
   
   names(activities) <- c("steps", 
@@ -70,14 +70,38 @@ getData <- function(token,
     httr::content(as = "text") |>
     jsonlite::fromJSON(simplifyVector = TRUE)
   
+  #heart data json response needs separate handlening
+  heart <- activities$heart
+  # keep <- heart
+  # heart <- keep
+  # names(heart$value$heartRateZones) <- heart$dateTime
+  dateTime <- heart$dateTime
+  heart <- lapply(c("Out of Range",
+                    "Fat Burn",
+                    "Cardio",
+                    "Peak"), \(property){
+                      propertyInt <- sapply(heart$value$heartRateZones, \(day){
+                        x <- day[day$name == property, -3] 
+                        colnames(x) <-  paste0(colnames(x)," ", property)
+                        x
+                      })
+                      t(propertyInt)
+                    })
+  heart <- Reduce(cbind, heart)
+  
+  #recast the data for one dataframe
+  activities <- activities[!grepl("heart", names(activities))]
   activities <- sapply(names(activities), \(act){
     if(!is.null(dim(activities[[act]]))){#sleep returns null sometimes
       colnames(activities[[act]])[-1] <- act
       activities[[act]]
     }else{
-      activities[[act]] <- data.frame(dateTime = activities[[1]][,1])
+      activities[[act]] <- data.frame(dateTime = activities[[1]][,1])#ugly hack for sleep being null
     }
   })
   
-  activities <- cbind(activities$heart, Reduce(, activities[-2]))
+  activities <- Reduce(cbind, activities)
+  activities <- activities[,!grepl("dateTime", colnames(activities))]
+  activities <- cbind(dateTime, heart, activities)
+  activities
 }
